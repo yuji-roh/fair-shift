acde209eb33fb7036fd00119bc7461b9df62651c
euijong <16833512+euijong@users.noreply.github.com>
2023-11-17 09:30:05 +0900

    Update models.py

--- models.py
+++ models.py
@@ -4,4 +4,26 @@ import torch.nn.functional as F
 
 from torch.autograd import Variable
 
+def weights_init_normal(m):
+    """Initializes the weight and bias of the model.
+
+    Args:
+        m: A torch model to initialize.
+
+    Returns:
+        None.
+    """
+    
+    classname = m.__class__.__name__
+    if classname.find("Conv") != -1:
+        torch.nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find("BatchNorm") != -1:
+        torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
+        torch.nn.init.constant_(m.bias.data, 0.0)
+    elif classname.find('Linear') != -1:
+        torch.nn.init.normal_(m.weight.data, 0.0, 0.2)
+        torch.nn.init.constant_(m.bias.data, 0)
+
+
+
 
